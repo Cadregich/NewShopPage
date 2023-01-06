2f3152bd8f8a09968da9835eddcc6f03d2b48434
Cadregich <theloyworkmail@gmail.com>
2023-01-06 21:39:03 +0200

    Create shop header panel

--- public/js/shop.js
+++ public/js/shop.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
+    //alert ($( document ).width());
     $('.low-screen-width-balance').hide();
-    if ($( document ).width() < 850) {
+    if ($( document ).width() < 1000) {
         $('.normal-screen-balance-block').hide();
         $('.low-screen-width-balance').show();
         $('.search-end-filter').css('justify-content', 'center');
--- resources/views/Shop.blade.php
+++ resources/views/Shop.blade.php
@@ -5,19 +5,19 @@
 @section('body')
     <i class="fa-regular fa-coin-blank"></i>
     <div class="container">
-
+        {{--    Low screen width balance panel--}}
         <div class="low-screen-width-balance">
             <div class="balance-low-screen">
                 <div class="balance-low-screen-balance">
                     Ваш баланс
                     <br>
-                    12344
+                    999999
                     <i class="fa-solid fa-coins"></i>
                 </div>
                 <button class="butt balance-butt">Пополнить</button>
             </div>
         </div>
-
+        {{--    ./Low screen width balance panel--}}
         <div class="shop-header-panel">
             <div class="shop-header-panel-body">
 
@@ -40,10 +40,33 @@
 
             </div>
         </div>
+    </div>
         <div class="cards-area">
+            @for ($i = 0; $i < 18; $i++)
+                <div class="card">
+                    <img src="{{ URL::asset('img/Hybrid_Solar_Panel_y2uSWXI.png') }}" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <div class="card-title">
+                                <center>
+                                    <h4>Гибридная солнечная панель</h4>
+                                </center>
+                        </div>
+                        <div class="card-text">
+                                Advanced solar panels
+                        </div>
+                        <center>
+                            <a href="#" class="btn butt card-btn">
+                                999
+                                <i class="fa-solid fa-coins"></i></a>
+                        </center>
+
+                    </div>
+                </div>
+            @endfor
 
         </div>
-    </div>
+
+
 @endsection
 @section('scripts')
     <script src="{{ asset('js/jquery-3.6.3.min.js') }}"></script>
